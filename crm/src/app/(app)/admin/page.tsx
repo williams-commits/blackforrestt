18835f0d97333233fc6bf9cdc15433c4877de80a
williams-commits <williamsmatthewj3@gmail.com>
2026-09-23 14:29:20 +0300

    fixed the admin

--- crm/src/app/(app)/admin/page.tsx
+++ crm/src/app/(app)/admin/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default function AdminIndexPage() {
-  redirect("/admin/statuses");
+  redirect("/admin/people");
 }
